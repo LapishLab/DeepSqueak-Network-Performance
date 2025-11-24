dda4fd340d9b2b177bf33646fd1544cad8ecba84
Brain1 <davidswygart@gmail.com>
2025-11-24 17:19:20 -0500

    handle no matching files

--- detection/detection_performance.m
+++ detection/detection_performance.m
@@ -76,19 +76,27 @@ function matches = find_matching_detection_files(truth, test)
     truth_files = string({dir(truth + filesep + "*.mat").name})';
     test_files = string({dir(test + filesep + "*.mat").name})';
 
-    matches = table();
-    matches.truth_file = truth + filesep + truth_files;
-    for i = 1:length(truth_files)
+    num_truth = length(truth_files);
+    matches = strings(num_truth,2);
+    match_found = false(num_truth,1);
+    for i = 1:num_truth
         pattern = extractBefore(truth_files(i), '.mat');
         is_match = contains(test_files, pattern);
         if sum(is_match)==1
-            matches.test_file(i)=test + filesep + test_files(is_match);
+            matches(i,1) = truth + filesep + truth_files(i);
+            matches(i,2) = test  + filesep + test_files(is_match);
+            match_found(i) = true;
         elseif sum(is_match)>1
             warning("Multiple test files found for " + pattern)
         elseif sum(is_match)<1
             warning("No test file found for " + pattern)
         end 
     end
-    matches = rmmissing(matches); % remove rows without matches
+
+    matches = matches(match_found,:);
+    if isempty(matches)
+        error("No matching files found in this folder")
+    end
+    matches = table(matches(:,1), matches(:,2), VariableNames=["truth_file","test_file"]);
 end
 
