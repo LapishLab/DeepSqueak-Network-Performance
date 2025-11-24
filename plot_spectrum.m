2007e88109c20475b490aec8295cb9586fc8562a
Brain1 <davidswygart@gmail.com>
2025-11-24 17:26:30 -0500

    Create plot_spectrum.m

---
+++ plot_spectrum.m
@@ -0,0 +1,35 @@
+function plot_spectrum(audio_file, start, stop)
+    [y, Fs] = load_audio_segment(audio_file, start, stop);
+    
+
+    window = round(Fs * 0.0032);% Deepsqueak default = .0032 s
+    nfft = round(Fs * 0.0032);% Deepsqueak default = .0032 s
+    % spectrogram(y,window,[],[], Fs, "yaxis")
+    [~,F,T,P] = spectrogram(y,window,[],nfft, Fs, 'power'); % defaults to 50% overlap with []
+    T = T+start;
+
+    P = sqrt(P);
+    % P = 10*log10(P+eps); %convert to decibel: Add eps to avoid log(0)
+  
+    imagesc(T, F/1000, P); 
+    axis xy;
+    ylabel('Frequency (kHz)');
+    xlabel('Time (s)');
+    c = colorbar;
+    c.Label.String = 'Amplitude';
+
+    colormap("inferno")
+
+    avg = mean(P,2); % amplitude above 40kHz 
+    dev = std(P,[],2);
+
+
+
+
+    low = min(avg);
+
+    shift_high = avg+(5*dev);
+    high = max(shift_high(F>4e4));
+
+    clim([low,high])
+end
