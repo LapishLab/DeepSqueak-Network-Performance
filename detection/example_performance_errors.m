95463f68218b9da5311daaee7ab0bc51c38110b3
Brain1 <davidswygart@gmail.com>
2025-12-09 13:49:24 -0500

    Create example_performance_errors.m

---
+++ detection/example_performance_errors.m
@@ -0,0 +1,41 @@
+%% Brandon vs. Anna (single file)
+brandon = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/train";
+anna = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/train/Anna_duplicate/";
+
+[score, details] = detection_performance(anna, brandon, .0001);
+
+% plot_FN(details)
+% plot_FP(details)
+plot_TP(details)
+
+%% Vary overlap for Brandon vs. Anna
+overlap = 0.01:.05:1;
+recalls = nan(size(overlap));
+precisions = nan(size(overlap));
+f1 = nan(size(overlap));
+for i = 1:length(overlap)
+    results = detection_performance(anna, brandon, overlap(i));
+    recalls(i) = results.recall;
+    precisions(i) = results.precision;
+    f1(i) = results.F1;
+end
+
+figure(1); clf; hold on;
+plot(overlap, recalls, DisplayName="recall")
+plot(overlap, precisions, DisplayName="precision")
+plot(overlap, f1, DisplayName="F1")
+legend()
+ylim([0 1])
+xlabel("Overlap")
+
+%% brandon vs brandon sanity check
+[results, details] = detection_performance(brandon, brandon, .1)
+
+%% Brandon vs. sara3 network
+brandon = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/validation/";
+sara3 = "/home/lapishla/Desktop/net_test/sara3/";
+[results, details] = detection_performance(brandon, sara3, .3)
+%% Brandon vs. Sulayman network (truitt.mat) 
+truitt = "/home/lapishla/Desktop/net_test/truitt/";
+[results, details] = detection_performance(brandon, truitt, .3)
+
