7b6059f02021fe1c2ae1462474767e78310fe5e4
Brain1 <davidswygart@gmail.com>
2025-12-18 20:52:19 -0500

    plot with duration threshold

--- detection/example_performance_errors.m
+++ detection/example_performance_errors.m
@@ -1,12 +1,34 @@
 %% Brandon vs. Anna (single file)
-brandon = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/train";
-anna = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/train/Anna_duplicate/";
-
-[score, details] = detection_performance(anna, brandon, .0001);
+brandon = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/validation";
+anna = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/validation/Anna_duplicate/";
+david = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/validation/David_duplicate/";
+% [score, details] = detection_performance(anna, brandon, .0001, 0.01)
+[score, details] = detection_performance(david, brandon, .0001, 0.01)
 
 % plot_FN(details)
-% plot_FP(details)
-plot_TP(details)
+plot_FP(details)
+% plot_TP(details)
+
+%% Vary USV duration threshold for Brandon vs. Anna
+duration = 0 : .005 : 1;
+recalls = nan(size(duration));
+precisions = nan(size(duration));
+f1 = nan(size(duration));
+for i = 1:length(duration)
+    results = detection_performance(david, brandon, .0001, duration(i));
+    recalls(i) = results.recall;
+    precisions(i) = results.precision;
+    f1(i) = results.F1;
+end
+
+figure(1); clf; hold on;
+plot(duration, recalls, DisplayName="recall")
+plot(duration, precisions, DisplayName="precision")
+plot(duration, f1, DisplayName="F1")
+legend()
+ylim([0 1])
+xlabel("duration (s)")
+
 
 %% Vary overlap for Brandon vs. Anna
 overlap = 0.01:.05:1;
