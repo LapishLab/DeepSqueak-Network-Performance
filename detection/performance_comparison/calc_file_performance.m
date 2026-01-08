2163fd12fbf1d2ae26c3ef332d949f86c51bc529
Brain1 <davidswygart@gmail.com>
2026-01-08 14:02:07 -0500

    Move overlap calculation to own file

--- detection/performance_comparison/calc_file_performance.m
+++ detection/performance_comparison/calc_file_performance.m
@@ -39,12 +39,5 @@ function s = calc_file_performance(truth_file, test_file, opts)
     s.FP = {FP_ind'}; 
 end
 
-function overlap_percentage = calc_box_overlap(A, B)
-% calculate the percent overlap of detection boxes
-    overlap_area = rectint(A, B);
-    A_area = A(:,3) .* A(:,4);
-    B_area = B(:,3) .* B(:,4);
-    total_area = A_area + B_area' - overlap_area;
-    overlap_percentage = overlap_area ./ total_area; 
-end
+
 
