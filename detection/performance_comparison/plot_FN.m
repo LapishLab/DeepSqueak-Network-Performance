7a07fd9fcade61c27d2fba3762b006e546bcf02d
Brain1 <davidswygart@gmail.com>
2026-01-08 14:41:39 -0500

    Display score and overlap when plotting FN

--- detection/performance_comparison/plot_FN.m
+++ detection/performance_comparison/plot_FN.m
@@ -5,14 +5,28 @@ function plot_FN(details)
         [~,name] = fileparts(details.truth_file(r));
         fprintf("Plotting %i False Negatives for %s \n", height(target_ind), name)
         
-        truth_boxes = load(details.truth_file(r)).Calls.Box;
-        test_boxes = load(details.test_file(r)).Calls.Box;
+        truth = load(details.truth_file(r)).Calls;
+        test = load(details.test_file(r)).Calls;
+        test.index = (1:height(test))';
         audio_file = load(details.truth_file(r)).audiodata.Filename;
         
         for i=1:height(target_ind)
-            target_box = truth_boxes(target_ind(i), :);
-            other_boxes = test_boxes;
-            plot_box_instance(target_box, other_boxes, audio_file)
+            missed_truth = truth(target_ind(i), :);
+            
+            test.overlap = calc_box_overlap(test.Box, missed_truth.Box);
+            
+            overlapping_test = test(test.overlap>0,:);
+
+            fprintf("  Missed USV index: %i\n", target_ind(i))
+            if ~isempty(overlapping_test)
+                fprintf("    Overlapping test boxes\n")
+                disp(overlapping_test)
+            else
+                fprintf("   No overlapping test boxes.\n");
+            end
+
+            plot_box_instance(missed_truth.Box, overlapping_test.Box, audio_file)
+
         end
     end
 end
