0e3e97f252204d91c2dd7cc4a94282c302841957
Brain1 <davidswygart@gmail.com>
2026-01-09 11:23:31 -0500

    print score and overlap for FP

--- detection/performance_comparison/plot_FP.m
+++ detection/performance_comparison/plot_FP.m
@@ -4,15 +4,36 @@ function plot_FP(details)
 
         [~,name] = fileparts(details.truth_file(r));
         fprintf("Plotting %i False Positives for %s \n", height(target_ind), name)
-        
-        truth_boxes = load(details.truth_file(r)).Calls.Box;
-        test_boxes = load(details.test_file(r)).Calls.Box;
+
+        truth = load(details.truth_file(r)).Calls;
+        test = load(details.test_file(r)).Calls;
+        test.index = (1:height(test))';
         audio_file = load(details.truth_file(r)).audiodata.Filename;
         
         for i=1:height(target_ind)
-            target_box = test_boxes(target_ind(i), :);
-            other_boxes = truth_boxes;
-            plot_box_instance(target_box, other_boxes, audio_file)
+            % target_box = test_boxes(target_ind(i), :);
+            % other_boxes = truth_boxes;
+            % plot_box_instance(target_box, other_boxes, audio_file)
+
+
+
+
+            bad_test = test(target_ind(i), :);
+            
+            truth.overlap = calc_box_overlap(truth.Box, bad_test.Box);
+            
+            overlapping_truth = truth(truth.overlap>0,:);
+
+            fprintf("  False positive at index: %i\n", target_ind(i))
+            disp(bad_test)
+            if ~isempty(overlapping_truth)
+                fprintf("    Overlapping truth boxes\n")
+                disp(overlapping_truth)
+            else
+                fprintf("   No overlapping truth boxes.\n");
+            end
+
+            plot_box_instance(bad_test.Box, overlapping_truth.Box, audio_file)
         end
     end
 end
