4737d7ff544fdcdf6a92685a00e8779daa29cfee
Brain1 <davidswygart@gmail.com>
2026-01-21 12:54:04 -0500

    add max and min frequency filtering

--- filter_calls.m
+++ filter_calls.m
@@ -4,15 +4,15 @@ function calls = filter_calls(calls, opts)
         opts.min_duration double = 0 % minimum duration of a USV to be included in the analysis
         opts.min_score double = 0 % Score (confidence) of a USV to be included in the analysis
         opts.include_rejected logical = false; % Should USVs marked as "rejected" be included in the analysis
+        opts.min_freq double = 18; % minimum frequency allowed for box
+        opts.max_freq double = 100; % maxiumum frequency allowed for box
     end
 
     % perform various checks
-    too_short = calls.Box(:,3) < opts.min_duration;
-    too_low_score = calls.Score < opts.min_score;
-
-    % Only keep calls that don't fail the above checks
-    is_good = ~too_short & ~too_low_score;
-    calls = calls(is_good,:);
+    calls = calls(calls.Box(:,3) > opts.min_duration, :);
+    calls = calls(calls.Score > opts.min_score, :);
+    calls = calls(calls.Box(:,2) > opts.min_freq, :);
+    calls = calls(calls.Box(:,2)+calls.Box(:,4) < opts.max_freq, :);
 
     % Only keep accepted calls
     if ~opts.include_rejected
