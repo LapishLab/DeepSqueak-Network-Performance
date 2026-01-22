162b47a7baa3ec43717db79dc6cb5b88fc77b80c
Brain1 <davidswygart@gmail.com>
2026-01-22 15:20:41 -0500

    just output intensity

--- detection/calculate_intensity.m
+++ detection/calculate_intensity.m
@@ -1,4 +1,4 @@
-function calls = calculate_intensity(calls, audiodata)
+function intensity = calculate_intensity(calls, audiodata)
 %TODO it would be much faster to precalculate once for each file. I can add
 %a check, but I also need the original mat filename in order to resave
 
@@ -13,6 +13,6 @@ function calls = calculate_intensity(calls, audiodata)
         segment = audio(time_inds(i, 1):time_inds(i, 2));
         rms_val(i) = sqrt(mean(segment.^2));
     end
-    calls.rms = rms_val;
 
+    intensity = rms_val;
 end
