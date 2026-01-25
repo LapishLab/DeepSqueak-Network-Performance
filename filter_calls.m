da1c14b250c19a5c38e8563f940932eb6c0889b5
Brain1 <davidswygart@gmail.com>
2026-01-24 20:46:25 -0500

    update default filtering

--- filter_calls.m
+++ filter_calls.m
@@ -1,8 +1,8 @@
 function detection = filter_calls(detection, opts)
     arguments
         detection struct %
-        opts.min_duration double = 0 % minimum duration of a USV to be included in the analysis
-        opts.min_score double = 0 % Score (confidence) of a USV to be included in the analysis
+        opts.min_duration double = .005 % minimum duration of a USV to be included in the analysis
+        opts.min_score double = 0.5 % Score (confidence) of a USV to be included in the analysis
         opts.include_rejected logical = false; % Should USVs marked as "rejected" be included in the analysis
         opts.min_freq double = 18; % minimum frequency allowed for box
         opts.max_freq double = 100; % maxiumum frequency allowed for box
