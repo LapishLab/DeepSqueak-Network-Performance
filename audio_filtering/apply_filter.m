281fdc2c653610cd93111b031afb8041b6fa261e
Brain1 <davidswygart@gmail.com>
2026-01-26 11:36:39 -0500

    use more efficient resampling for filtering resampling

--- audio_filtering/apply_filter.m
+++ audio_filtering/apply_filter.m
@@ -4,8 +4,10 @@ function audio_out = apply_filter(audio_in, filter)
 X = fftshift(fft(audio_in)); % fftshift so that 0 frequency in middle
 
 %% upsample filter to match fft and apply
-big_filter = resample(filter,length(X),length(filter)); 
-Y = X .* big_filter; % Apply filter in frequency domain
+little_x = linspace(0,1, length(filter));
+bix_x = linspace(0,1, length(X));
+big_filter = interp1(little_x, filter,bix_x, "linear");
+Y = X(:) .* big_filter(:); % Apply filter in frequency domain
 
 %% invert filtered fft back to time domain and normalize
 audio_out = real(ifft(fftshift(Y)));
