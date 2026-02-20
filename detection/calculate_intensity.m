e0f0073ae7f9682e75acb28e087a15e48633664b
Brain1 <davidswygart@gmail.com>
2026-02-20 11:33:58 -0500

    Use ridge for call intensity

--- detection/calculate_intensity.m
+++ detection/calculate_intensity.m
@@ -1,18 +1,34 @@
-function intensity = calculate_intensity(calls, audiodata)
+function avg_call_intensity = calculate_intensity(calls, audiodata)
 %TODO it would be much faster to precalculate once for each file. I can add
 %a check, but I also need the original mat filename in order to resave
 
-    [audio,Fs] = audioread(audiodata.Filename);
+    avg_call_intensity = nan(height(calls),1);
+    for i=1:height(calls)
+        start_time = calls.Box(i,1);
+        stop_time = sum(calls.Box(i,[1,3]));
 
-    time_inds = calls.Box(:,[1,3])*Fs;
-    time_inds(:,2) = sum(time_inds,2);
-    time_inds = round(time_inds);
+        figure(1); clf; hold on; 
+        [F,T,P] = plot_spectrum(audiodata.Filename, start_time, stop_time);
+        rectangle(Position=calls.Box(i,:), EdgeColor='white')
 
-    rms_val = nan(height(calls),1);
-    for i = 1:height(calls)
-        segment = audio(time_inds(i, 1):time_inds(i, 2));
-        rms_val(i) = sqrt(mean(segment.^2));
+        min_freq = calls.Box(i,2) * 1000;
+        max_freq = sum(calls.Box(i,[2,4])) * 1000;
+        box_freq_inds = F>min_freq & F<max_freq;
+
+        F = F(box_freq_inds);
+        P = P(box_freq_inds,:);
+
+        [amplitude, freq_inds, time_inds] = detect_ridge(P);
+
+
+        x = T(time_inds);
+        y = F(freq_inds) / 1000; %F is in Hz, convert to kHz
+        scatter(x, y, 'filled', 'green')
+        
+        % average the final pixel values
+        avg_call_intensity(i) = mean(amplitude); % Store the maximum intensity for the current call
     end
 
-    intensity = rms_val;
+    figure(2);clf;
+    histogram(avg_call_intensity)
 end
