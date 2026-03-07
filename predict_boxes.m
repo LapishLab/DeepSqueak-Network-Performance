69f56233ef2037b6c61db69c0b85e8669edfbbf1
Brain1 <davidswygart@gmail.com>
2026-03-06 23:38:23 -0500

    predict boxes use Call labels instead of img table

--- predict_boxes.m
+++ predict_boxes.m
@@ -4,7 +4,8 @@ function prediction = predict_boxes(im, detector)
     labels = labels(ind);
 
     prediction = table();
-    prediction.Boxes = bboxes;
-    prediction.Scores = scores;
-    prediction.Labels = labels;
+    prediction.Box = bboxes;
+    prediction.Score = scores;
+    prediction.Type = labels;
+    prediction.Accept = ones(length(labels),1);
 end
