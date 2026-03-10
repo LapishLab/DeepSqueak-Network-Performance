47c27c71c9cafec9bcc73aaf817f08f5b39dc46f
Brain1 <davidswygart@gmail.com>
2026-03-10 17:33:27 -0400

    update to skip completed files

--- batch_detect_calls.m
+++ batch_detect_calls.m
@@ -1,15 +1,24 @@
 function batch_detect_calls(audio_folder, output_folder, network)
-    %% Get path to all audio files
-    fnames = {dir(fullfile(audio_folder,"*.flac")).name};
-    audio_files = fullfile(audio_folder, fnames)';
-    for i = 1:length(fnames)
+    % make a table to keep track of files
+    t = table();
+
+    %% Get audio files and planned save names
+    t.audio_names = string({dir(fullfile(audio_folder,"*.flac")).name})';
+    t.audio_paths = fullfile(audio_folder, t.audio_names);
+    t.mat_names = strrep(t.audio_names, ".flac", ".mat");
+    t.mat_paths = fullfile(output_folder,t.mat_names);
+    
+    %% Check that file hasn't already been processed
+    need_export = ~cellfun(@exist, t.mat_paths);
+    t = t(need_export,:);
+    
+    %% Run detection on each file and save results in mat
+    for i = 1:height(t)
         % Run detection
-        detection = detect_calls(audio_files(i), network);
+        detection = detect_calls(t.audio_paths(i), network);
     
         % Save detection to mat file
-        [~,subname,~] = fileparts(audio_files(i));
-        file_path = fullfile(output_folder, subname+".mat");
-        save(file_path, '-struct', "detection")
-        fprintf("Completed file %i/%i \n", i,length(fnames))
+        save(t.mat_paths(i), '-struct', "detection")
+        fprintf("Completed file %i/%i \n", i,height(t))
     end
 end
