c872581b7ce9f6445707d06811a0e8a40ae9d88f
Brain1 <davidswygart@gmail.com>
2026-03-17 17:38:43 -0400

    swygart squeaks working example

--- detection/trainExample.m
+++ detection/trainExample.m
@@ -3,7 +3,6 @@ clear
 % train = "/home/lapishla/Desktop/training/training_images/";
 % validate = "/home/lapishla/Desktop/training/validation_images/";
 
-
 train = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/train/";
 validate = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/detection_files/validation/";
 
@@ -12,15 +11,31 @@ validate_img = "/home/lapishla/Desktop/test/validate";
 
 settings = spectrogram_settings();
 %% Create training images
-% train_info = create_training_images(train,train_img,settings, saveAnnotated=true);
-val_info = create_training_images(validate,validate_img,settings, saveAnnotated=true);
+create_training_images(train,train_img,settings);
+create_training_images(validate,validate_img,settings);
 %% Make a fresh detector
-net = "/home/lapishla/Documents/GitHub/DeepSqueak/Networks/freshYOLOX.mat";
-detector = generate_blank_YOLOX(net, train_info);
-%% Train the detector
-[detector, info, options] = train_detector(train_img, validate_img, net);
-%% Run validation on the generated images
 im_table = load(fullfile(validate_img, 'img_table.mat'));
+labels=unique(cat(1,im_table.TTable.Labels{:}));
+net = "/home/lapishla/Documents/GitHub/DeepSqueak/Networks/freshYOLOX.mat";
+generate_blank_YOLOX(net, settings, labels);
+%% Train the detector
+network = train_detector(train_img, validate_img, net);
+%% Run validation on the generated images
+[score,details] = detect_pregenerated_images(network.detector,im_table);
+%% run detector
+network = load("/home/lapishla/Documents/GitHub/DeepSqueak/Networks/YOLOX3_2026-03-03_09-31-34.mat");
+network.settings = spectrogram_settings();
 %%
-[score,details] = detect_pregenerated_images(detector,im_table)
-
+prediction_output = "/home/lapishla/Desktop/Prat_all_predictions/";
+network = load("/home/lapishla/Documents/GitHub/DeepSqueak/Networks/YOLOX3_2026-03-03_09-31-34.mat");
+network.settings = spectrogram_settings();
+audio_root = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/audio/";
+%%
+audio_folder = audio_root + "train/";
+batch_detect_calls(audio_folder, prediction_output, network)
+%%
+audio_folder = audio_root + "test/";
+batch_detect_calls(audio_folder, prediction_output, network)
+%%
+audio_folder = audio_root + "validation/";
+batch_detect_calls(audio_folder, prediction_output, network)
