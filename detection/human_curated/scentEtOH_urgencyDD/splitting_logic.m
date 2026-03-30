dd8d470cccd758ac48bdf7b7dece6a904cc1a75f
Brain1 <davidswygart@gmail.com>
2026-03-30 17:51:42 -0400

    urgency DD specific functions

--- detection/human_curated/scentEtOH_urgencyDD/splitting_logic.m
+++ detection/human_curated/scentEtOH_urgencyDD/splitting_logic.m
@@ -1,6 +1,9 @@
 %% load csv 
 clear
-t = readtable("/home/lapishla/Desktop/DD_audioFiles.csv", Delimiter=',');
+%csv to the files that have datastar paths to each file as well as
+%identifying information about that file that you want to use to assign
+%groups (ex/ subject number, sex, issueTime, reinforcer)
+t = readtable("/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/scentEtOH_urgencyDD/audioPaths_subjectInfo.csv", Delimiter=',');
 original = t; % save an unedited copy of the table
 t = convertvars(t, @(x) true, "string"); % convert everything to strings because David likes them more than cell arrays of characters
 
@@ -8,7 +11,7 @@ t = convertvars(t, @(x) true, "string"); % convert everything to strings because
 [~,f_names,~] = fileparts(t.audio_file_path);
 file_times = datetime(f_names, 'InputFormat', 'yyyyMMdd_HHmmss');
 t.issueTime = pad(t.issueTime, 6, 'left', '0');
-issue_times = datetime(t.issueTime, 'InputFormat', 'HHmmss');  % Some missing, marked as NaN
+issue_times = datetime(t.issueTime, 'InputFormat', 'HHmmss');% Some missing, marked as NaN
 post_issue = timeofday(file_times)-timeofday(issue_times); % need to use timeofday because date not included in issueTime
 post_issue = seconds(post_issue); % convert to seconds for easier comparison
 t.post_issue = post_issue;
@@ -17,27 +20,17 @@ t.post_issue = post_issue;
  % ylabel('File counts')
  % xlabel('Audio file start time relative to MedPC issue time (s)')
 
-
 %% Divide session time into thirds (find divide times)
 % another option would 
- min_time = -3 * 60; % allow down to -5 minutes as this is the max file length.
- max_time = 60 * 60; % Session should be max 60 minutes
+ min_time = -10 * 60; % allow down to -# minutes as this is the max file length. Increased the time since these files are longer 
+ max_time = 60 * 60; % Session should be max # minutes. Can change based on what behavior this is being used for 
  ecdf(post_issue(post_issue>min_time & post_issue<max_time))
  yline(0.333, '--')
  yline(0.666, '--')
+
+
 
 tdiv = [min_time 900 2250  max_time];
-
-%% Divide session time into thirds: DD %%
- min_time = -3 * 60; % allow down to -5 minutes as this is the max file length.
- max_time = 30 * 60; % Session should be max 60 minutes
- ecdf(post_issue(post_issue>min_time & post_issue<max_time))
- yline(0.333, '--')
- yline(0.666, '--')
- %time length divided by a third
-third_time = (abs(min_time) + abs(max_time))/3;
-
-tdiv = [min_time (min_time+third_time) (max_time-third_time)  max_time];
 
 %% Divide session time into thirds (Assign time labels)
 t.time_group = strings(height(t),1);
@@ -55,8 +48,9 @@ t{contains(t.treatment, "EtOH"), "treatment"} = "EtOH_EtOH"; % Treat single rat 
 %% choose which rows to include
 include_row = t.time_group ~= ""; % don't include rows that were not assigned a time label
 
+
 %% Calculate balanced split for given rows and columns
-[split, group_id] = balanced_split(t(include_row, {'sex','treatment','time_group'}));
+[split, group_id] = balanced_split(t(include_row, {'sex','treatment','time_group', 'scentDays'}));
 
 %% Add time group, split, and unique group ID to the original table
 original.time_group = t.time_group;
@@ -64,5 +58,5 @@ original.split(include_row) = split;
 original.group_id(include_row) = group_id;
 
 %% Save the table
-writetable(original, 'split.csv')
+writetable(original, "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/scentEtOH_urgencyDD/split.csv")
 
